fed9d8155439d89772caa6ad1241e5c1235a31ab
stevil <code.stephen@gmail.com>
2016-06-28 12:53:15 +0100

    more system info, memory

--- camwipe.py
+++ camwipe.py
@@ -5,9 +5,20 @@ This script is used for bulk drive erase in addition to wiping single disks in
 individual machines."""
 
 import datetime
+import subprocess
+
+class SystemInfo:
+    """Placeholder for system settings and information."""
+
+
+def call(cmd):
+    return subprocess.check_output(cmd, shell=True)
+
+
 
 if __name__ == '__main__':
     print 'Camara Education disk wiping tool'
+
 
     print(datetime.datetime.now())
 
@@ -15,6 +26,19 @@ if __name__ == '__main__':
     barcode = ''.join(c for c in barcode if c.isalnum())
 
     print(barcode)
+
+    system_info = SystemInfo()
+
+    system_info.nwipe_version = call('nwipe --version')
+    print system_info.nwipe_version
+    system_info.free_memory = int(call("free | grep -w 'Mem' | awk '{print $2}'"))/1024
+    print system_info.free_memory
+    system_info.memsys = call("lshw -C memory -short | grep 'System' | awk '{print $3}'")
+    print system_info.memsys
+
+
+
+
 
 
 
